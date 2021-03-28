c3e7ab857b396fc709fe574d21ce23d45c6b5353
seantyh <seantyh@gmail.com>
2021-03-28 21:44:35 +0800

    add model and negative sampling

--- src/fluidvec/dataset.py
+++ src/fluidvec/dataset.py
@@ -4,9 +4,12 @@ import pickle
 from torch.utils.data import Dataset, DataLoader
 from torch.nn.utils.rnn import pad_sequence
 
+def collate_fn(xs):
+    return xs
+
 def get_dataloader(fpath, batch_size=8):
     ds = TrainDataset(fpath)
-    loader = DataLoader(ds, batch_size=batch_size)
+    loader = DataLoader(ds, batch_size=batch_size, collate_fn=collate_fn)
     return loader
 
 class TrainDataset(Dataset):
@@ -18,21 +21,6 @@ class TrainDataset(Dataset):
     def __len__(self):
         return len(self.data)
     
-    def pad_tensor(self, values):
-        n = max(len(x) for x in values)
-        w = len(values)
-        t = np.ones((w, n))  # padding value is 1
-        for i, v in enumerate(values):
-            t[i, :len(v)] = v
-        return torch.tensor(t, dtype=torch.int32)
-    
     def __getitem__(self, idx):
-        tgt, ctx = self.data[idx]
-        return {
-            "tgt_word": torch.tensor(tgt["word"]).reshape(1, 1),
-            "tgt_chars": torch.tensor(tgt["chars"]).reshape(1, -1),
-            "tgt_compos": torch.tensor(tgt["compos"]).reshape(1, -1),
-            "ctx_word": torch.tensor([x["word"] for x in ctx]).reshape(len(ctx), -1),
-            "ctx_chars": [x["chars"] for x in ctx],
-            "ctx_compos": [x["compos"] for x in ctx]
-        }
+        return self.data[idx]
+        
--- src/fluidvec/model.py
+++ src/fluidvec/model.py
@@ -2,15 +2,21 @@ import torch
 import torch.nn as nn
 
 class FluidVecSG(nn.Module):
-    def __init__(self, n_word, n_char, n_compo, dim=300, winsize=4):
+    def __init__(self, n_word, n_char, n_compo, dim=300, winsize=4, 
+            weights=None, neg_sample=.75):
         super(FluidVecSG, self).__init__()
         self.word_emb = nn.Embedding(n_word, dim)
         self.char_emb = nn.Embedding(n_char, dim)
         self.compo_emb = nn.Embedding(n_compo, dim)
-    
-    def forward(self, batch_data):
-        pass
+        if not weights:
+            self.weights = torch.ones(n_word)
+        else:
+            self.weights = torch.tensor(weights)
+        self.n_neg_sample = int(n_word ** neg_sample)
 
+    def forward(self, batch_data):
+        return self.loss(batch_data)
+        
     def transform_batch_data(self, batch_data):
         # in skip-gram, we use target chars and compos to predict context words
         tgt_data = [x[0] for x in batch_data]  # Entry x batch_size
@@ -51,3 +57,25 @@ class FluidVecSG(nn.Module):
         else:
             vec = torch.zeros(emb.embedding_dim)
         return vec
+    
+    def sample(self, num_sample):
+        return 
+
+    def loss(self, batch_data):
+        vec_dict = self.transform_batch_data(batch_data)
+        tgt = vec_dict["tgt"]
+        ctx = vec_dict["ctx"]
+
+        batch_size = ctx.size(0)
+        win_size = ctx.size(1)
+        n_noise = batch_size * win_size * self.n_neg_sample
+        draw = torch.multinomial(self.weights, n_noise, True)
+        noise = draw.view(batch_size, win_size, self.n_neg_sample)
+        
+        log_target = (tgt.unsqueeze(1) * ctx).sum(2).sigmoid().log().sum()
+        sum_log_noise = ((tgt.unsqueeze(1).unsqueeze(1)*noise)
+                        .neg().sum(3).sigmoid()+1e-32).log().sum()
+        loss = log_target + sum_log_noise
+
+        return -loss / batch_size
+
